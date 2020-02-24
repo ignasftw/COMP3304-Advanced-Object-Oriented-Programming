2b8d5f3fe675ed259a5d2fcac75ff50b91394622
ignasftw <32689847+ignasftw@users.noreply.github.com>
2020-02-24 15:22:02 +0000

    AutoScale Fix
    It was because the image method was changed from void to Image and someone perhaps forgot updating all methods. Anyway that is fixed now. I am glad we are doing great job. :+1:

--- ImagePinkifier/ImagePinkifier/ImagePinkifier.cs
+++ ImagePinkifier/ImagePinkifier/ImagePinkifier.cs
@@ -64,7 +64,7 @@ namespace WindowsFormsApp1
 
         private void zoomAutoButton_Click(object sender, EventArgs e)
         {
-            _scaling.autoResizeImage(pictureBox.Size, _imfac);
+            pictureBox.Image = _scaling.autoResizeImage(pictureBox.Size, _imfac);
         }
 
         private void zoomInButton_Click(object sender, EventArgs e)
