6294303111cf7c68a37d3f23a98954c7496f1e99
grimer30 <mstout64@gmail.com>
2020-02-24 10:43:41 +0000

    why wasn't the library in here i made it back

---
+++ SizeMultiplier/SizeMultiplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeMultiplier
+{
+    public static class SizeMultiplier
+    {
+        public static Size Multiply(this Size size, double factor)
+        {
+            System.Diagnostics.Debug.WriteLine(size.Width);
+            return new Size((int)(size.Width * factor), (int)(size.Height * factor));
+        }
+    }
+}
